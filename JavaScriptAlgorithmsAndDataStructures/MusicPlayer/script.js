3e1252ce4544fe074b8a7643eb3267a5f9e35052
crabstechnologies <ata_noshadi@yahoo.com>
2025-01-01 18:59:28 +0100

    36% done

--- JavaScriptAlgorithmsAndDataStructures/MusicPlayer/script.js
+++ JavaScriptAlgorithmsAndDataStructures/MusicPlayer/script.js
@@ -79,11 +79,14 @@ const allSongs = [
 ];
 
 const audio = new Audio();
-
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
+};
+
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
 };
 
 const renderSongs = (array) => {
@@ -105,5 +108,23 @@ const renderSongs = (array) => {
     })
     .join("");
 
-
+  playlistSongs.innerHTML = songsHTML;
 };
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
